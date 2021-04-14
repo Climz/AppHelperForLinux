f4a1c67528d38ed2418ad1e19927d4f7228a3994
Climz <1683613list@gmail.com>
2021-04-15 01:24:33 +0300

    Changed work of add app, added input area by pressed button "Add",

--- main.py
+++ main.py
@@ -1,63 +1,89 @@
 from tkinter import *
 from tkinter.ttk import Combobox, Checkbutton, Button
 from PIL import ImageTk, Image
+import os
 
 
 window = Tk()
 window.title("AppHelper")
 
-chk_state = IntVar() # Значение по умолчанию
+chk_state = IntVar()
 window.minsize("200","100")
 
-def addapp(awe):
+def addapp(ae):
 	with open("appfile.txt", "a") as f:
-		f.write(awe.get() + "\n")
+		f.write(ae.get() + "\n")
+	ae.delete(0, END)
+
+
+def addapp_cancel(entry, add, cancel):
+	entry.grid_remove()
+	add.grid_remove()
+	cancel.grid_remove()
 
 
 def F():
 	pass
 
 
+def select_all():
+	for i in IntVar_list:
+		i.set(1)
+
+
+def deselect_all():
+	for i in IntVar_list:
+		i.set(0)
+
+
 def addapp_button():
-	# with open ("appfile.txt", "a") as f:
-	# 	f.write("F")
-	addapp_window = Tk()
-	addapp_window.title("Add app")
+	# addapp_window = Tk()
+	# addapp_window.title("Add app")
 
-	addapp_window_entry = Entry(addapp_window)
-	addapp_window_entry.grid(column=1, columnspan=2, row=1)
+	# addapp_window_entry = Entry(addapp_window)
+	# addapp_window_entry.grid(column=1, columnspan=2, row=1)
 
-	addapp_window_button_ADD = Button(addapp_window, text="Add", command= lambda: addapp(addapp_window_entry))
-	addapp_window_button_ADD.grid(column=1, row=2)
+	# addapp_window_button_ADD = Button(addapp_window, text="Add", command= lambda: addapp(addapp_window_entry))
+	# addapp_window_button_ADD.grid(column=1, row=2)
 
-	addapp_window_button_CANCEL = Button(addapp_window, text="Cancel", command=addapp_window.quit)
-	addapp_window_button_CANCEL.grid(column=2, row=2)
+	# addapp_window_button_CANCEL = Button(addapp_window, text="Cancel", command=addapp_window.quit)
+	# addapp_window_button_CANCEL.grid(column=2, row=2)
 
-	addapp_window.mainloop()
+	# addapp_window.mainloop()
+
+	addapp_entry = Entry(window, fg="#000000", bg="#FFFFFF")
+	addapp_entry.grid(column=last_i+2, row=5, columnspan=last_i+3, padx=(50,0), sticky=W+E)
+	addapp_button_ADD = Button(window, text="Add", command=lambda: addapp(addapp_entry))
+	addapp_button_ADD.grid(column=last_i+2, row=6, padx=(50,0), sticky=W+E)
+	addapp_button_CANCEL = Button(window, text="Cancel", command=lambda: addapp_cancel(addapp_entry,addapp_button_ADD,addapp_button_CANCEL))
+	addapp_button_CANCEL.grid(column=last_i+3, row=6, sticky=W+E)
 
 
 
 def letsgo():
 	what_download_list = []
+	what_distr = distr_cb.get()
 	for i in range(app_list_len):
 		IntVar_list_v = IntVar_list[i].get()
 		print(app_list[i] + "\t" + str(IntVar_list_v))
 		if IntVar_list_v == 1:
 			what_download_list.append(app_list[i])
-	print(what_download_list)
+	print(" ".join(what_download_list))
+	if what_distr == "Archlinux":
+		os.system("sudo pacman -S " + " ".join(what_download_list))
+	elif what_distr == "Ubuntu":
+		os.system("sudo apt install " + " ".join(what_download_list))
+
+
 app_list = []
 with open ("appfile.txt", "r") as f:
 	l = f.read()
 	l = l.replace("\n",",")
 	app_list = l.split(",")
 
-# app_list = ["flameshot", "git", "htop", "pip", "qttools5-dev",
-# "qttools5-dev-tools", "obs-studio", "neofetch", "latte-dock", 
-# "google-chrome-stable", "discord", "spotify-client", "postgresql", 
-# "default-jre", "default-jdk", "gimp", "libreoffice", "python3-tk", "inkscape"]
+
 app_list.pop()
 app_list_len = len(app_list)
-
 IntVar_list = []
 Checkbutton_list = []
 Checkbutton_label_list = []
@@ -65,6 +91,7 @@ for i in range(app_list_len):
 	IntVar_list.append(IntVar())
 	Checkbutton_list.append(Checkbutton(window, variable=IntVar_list[i]))
 	Checkbutton_label_list.append(Label(window, text=app_list[i]))
+
 
 last_i = 0
 for i in range(app_list_len):
@@ -77,50 +104,27 @@ for i in range(app_list_len):
 		last_i = 2
 		Checkbutton_label_list[i].grid(column=2, row=i+1, sticky=W)
 
-# for i in app_list:
-# 	row_num = app_list.index(i)
-# 	print(row_num)
-	
 
-# 	chk = Checkbutton(window, variable=IntVar_list[app_list.index(i)], onvalue=1, offvalue=0, command=F)
-# 	chk.grid(column=1, row=row_num+1, sticky=W)
-	
-# 	label = Label(window, text=i)
-# 	label.grid(column=2, row=row_num+1, sticky=W)
-
-# 	print(chk, label)
-
-
-cb = Combobox(window, values=["Archlinux", "Ubuntu"], width=10)
-cb.grid(column=last_i+2, row=2, padx=50, sticky=W+E)
+distr_list = ["Archlinux", "Ubuntu"]
+distr_cb = Combobox(window, values=distr_list, width=10)
+distr_cb.set(distr_list[0])
+distr_cb.grid(column=last_i+2, columnspan=last_i+3, row=2, padx=(50,0), sticky=W+E)
 
 label = Label(window, text="Choose dist")
-label.grid(column=last_i+2, row=1, sticky=W, padx=50)
+label.grid(column=last_i+2, columnspan=last_i+3, row=1, padx=(50,0))
 
 #Last button to start proces
 final_button = Button(window, text="Let's go", command=letsgo)
-final_button.grid(column=last_i+2, row=3, padx=50, sticky=W+E)
+final_button.grid(column=last_i+2, row=3, padx=(50,0), sticky=W+E)
 
 addapp_button = Button(window, text="Add app", command=addapp_button)
-addapp_button.grid(column=last_i+2, row=4, padx=50, sticky=W+E)
+addapp_button.grid(column=last_i+3, row=3, sticky=W+E)
 
+select_all_button = Button(window, text="Select all", command=select_all)
+select_all_button.grid(column=last_i+2, row=4, padx=(50,0), sticky=W+E)
 
-# chk1 = Checkbutton(window)
-# chk1.grid(column=1, row=1)
-# chk2 = Checkbutton(window)
-# chk2.grid(column=1, row=2)
-# chk3 = Checkbutton(window)
-# chk3.grid(column=1, row=3)
-# chk4 = Checkbutton(window)
-# chk4.grid(column=1, row=4)
+deselect_all_button = Button(window, text="Deselect all", command=deselect_all)
+deselect_all_button.grid(column=last_i+3, row=4, sticky=W+E)
 
-# label1 = Label(window, text="HTOP")
-# label1.grid(column=2, row=1)
-# label2 = Label(window, text="Flameshot")
-# label2.grid(column=2, row=2)
-# label3 = Label(window, text="GIT")
-# label3.grid(column=2, row=3)
-# label4 = Label(window, text="Latte-dock")
-# label4.grid(column=2, row=4)
 
 window.mainloop()
