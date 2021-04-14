9afa66078603d9ee1b7788c8d292941ac436e012
Climz <1683613list@gmail.com>
2021-04-14 18:37:07 +0300

    Added functions for add apps, write choosen apps to list

--- main.py
+++ main.py
@@ -9,15 +9,42 @@ window.title("AppHelper")
 chk_state = IntVar() # Значение по умолчанию
 window.minsize("200","100")
 
+def addapp(awe):
+	with open("appfile.txt", "a") as f:
+		f.write(awe.get() + "\n")
 
-def addapp():
-	with open ("appfile.txt", "a") as f:
-		f.write("F")
+
+def F():
+	pass
+
+
+def addapp_button():
+	# with open ("appfile.txt", "a") as f:
+	# 	f.write("F")
+	addapp_window = Tk()
+	addapp_window.title("Add app")
+
+	addapp_window_entry = Entry(addapp_window)
+	addapp_window_entry.grid(column=1, columnspan=2, row=1)
+
+	addapp_window_button_ADD = Button(addapp_window, text="Add", command= lambda: addapp(addapp_window_entry))
+	addapp_window_button_ADD.grid(column=1, row=2)
+
+	addapp_window_button_CANCEL = Button(addapp_window, text="Cancel", command=addapp_window.quit)
+	addapp_window_button_CANCEL.grid(column=2, row=2)
+
+	addapp_window.mainloop()
+
+
+
 def letsgo():
-	print(cb.get())
+	what_download_list = []
 	for i in range(app_list_len):
-		print(IntVar_list[i].get())
-
+		IntVar_list_v = IntVar_list[i].get()
+		print(app_list[i] + "\t" + str(IntVar_list_v))
+		if IntVar_list_v == 1:
+			what_download_list.append(app_list[i])
+	print(what_download_list)
 app_list = []
 with open ("appfile.txt", "r") as f:
 	l = f.read()
@@ -28,7 +55,7 @@ with open ("appfile.txt", "r") as f:
 # "qttools5-dev-tools", "obs-studio", "neofetch", "latte-dock", 
 # "google-chrome-stable", "discord", "spotify-client", "postgresql", 
 # "default-jre", "default-jdk", "gimp", "libreoffice", "python3-tk", "inkscape"]
-
+app_list.pop()
 app_list_len = len(app_list)
 
 IntVar_list = []
@@ -39,12 +66,16 @@ for i in range(app_list_len):
 	Checkbutton_list.append(Checkbutton(window, variable=IntVar_list[i]))
 	Checkbutton_label_list.append(Label(window, text=app_list[i]))
 
-
+last_i = 0
 for i in range(app_list_len):
-
-	Checkbutton_list[i].grid(column=1, row=i+1, sticky=W)
-
-	Checkbutton_label_list[i].grid(column=2, row=i+1, sticky=W)
+	if i >= 19 and i <= 39:
+		Checkbutton_list[i].grid(column=3, row=i-18, sticky=W)
+		last_i = 4
+		Checkbutton_label_list[i].grid(column=4, row=i-18, sticky=W)
+	else:
+		Checkbutton_list[i].grid(column=1, row=i+1, sticky=W)
+		last_i = 2
+		Checkbutton_label_list[i].grid(column=2, row=i+1, sticky=W)
 
 # for i in app_list:
 # 	row_num = app_list.index(i)
@@ -61,17 +92,17 @@ for i in range(app_list_len):
 
 
 cb = Combobox(window, values=["Archlinux", "Ubuntu"], width=10)
-cb.grid(column=3, row=2)
+cb.grid(column=last_i+2, row=2, padx=50, sticky=W+E)
 
 label = Label(window, text="Choose dist")
-label.grid(column=3, row=1, sticky=W)
+label.grid(column=last_i+2, row=1, sticky=W, padx=50)
 
 #Last button to start proces
 final_button = Button(window, text="Let's go", command=letsgo)
-final_button.grid(column=3, row=3)
+final_button.grid(column=last_i+2, row=3, padx=50, sticky=W+E)
 
-addapp_button = Button(window, text="Add app", command=addapp)
-addapp_button.grid(column=3, row=4)
+addapp_button = Button(window, text="Add app", command=addapp_button)
+addapp_button.grid(column=last_i+2, row=4, padx=50, sticky=W+E)
 
 
 # chk1 = Checkbutton(window)
